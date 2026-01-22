d681689368dbf9018bb2da3a3746c4e93d0fad37
Esteban García Marín <esgarma93@gmail.com>
2026-01-21 23:19:37 -0500

    Add dist to gitignore

--- dist/partners/application/dto/partner-response.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PartnerResponseDto = void 0;
-class PartnerResponseDto {
-}
-exports.PartnerResponseDto = PartnerResponseDto;
-//# sourceMappingURL=partner-response.dto.js.map
--- dist/partners/domain/partner.repository.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PARTNER_REPOSITORY = void 0;
-exports.PARTNER_REPOSITORY = 'PARTNER_REPOSITORY';
-//# sourceMappingURL=partner.repository.js.map
