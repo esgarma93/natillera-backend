1072853c75d92a75e1c10aebc1b07a4e30c0061d
Esteban García Marín <esgarma93@gmail.com>
2026-01-21 23:19:37 -0500

    Add dist to gitignore

--- dist/payments/application/dto/payment-response.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PaymentResponseDto = void 0;
-class PaymentResponseDto {
-}
-exports.PaymentResponseDto = PaymentResponseDto;
-//# sourceMappingURL=payment-response.dto.js.map
